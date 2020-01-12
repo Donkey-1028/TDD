7386144ec4bbfdd92c0e92f0a4ba554b295b0538
Donkey-1028 <anbo10283182@gmail.com>
2020-01-12 18:24:10 +0900

    Django TestCase 작성

--- functional_tests.py
+++ functional_tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://127.0.0.1:8000')
 
         # 웹 페이지 타이틀과 헤더가 'To-Do' 를 표시하고 있따
-        self.assertIn('To-DO', self.browser.title)
+        self.assertIn('To-Do lists', self.browser.title)
         self.fail('Finish the test!')
 
     if __name__ == '__main__':
