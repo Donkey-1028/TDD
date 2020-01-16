337f44067c9fbacbc5c553c7da6ccabdf0d002e2
Donkey-1028 <anbo10283182@gmail.com>
2020-01-16 15:37:56 +0900

    List 모델 생성 및 조정

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 
-from .models import Item
+from .models import Item, List
 # Create your views here.
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 
 
