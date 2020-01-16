b7fe19258560ea8b70b828bbd3ec62bae9d06649
Donkey-1028 <anbo10283182@gmail.com>
2020-01-16 13:40:01 +0900

    functinal_tests 앱 생성 및 LiveServerTestCase 사용

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome('C:/chromedriver.exe')
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get(self.live_server_url)
 
         # 웹 페이지 타이틀과 헤더가 'To-Do' 를 표시하고 있따
         self.assertIn('To-Do lists', self.browser.title)
@@ -49,9 +50,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
         inputbox.send_keys(Keys.ENTER)
 
-        #페이지는 다시 갱신되고 두 개 아이템이 목록에 보인다
+        # 페이지는 다시 갱신되고 두 개 아이템이 목록에 보인다
         self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
-    if __name__ == '__main__':
-        unittest.main(warnings='ignore')
+        self.fail('Finish the test !')
