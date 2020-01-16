b1119fc94a86a68013d3719589d5239ea93b464a
Donkey-1028 <anbo10283182@gmail.com>
2020-01-16 14:36:07 +0900

    목록 출력을 위한 신규 URL, 뷰

--- lists/urls.py
+++ lists/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home_page
+from .views import home_page, view_list
 
 app_name = 'lists'
 
 urlpatterns = [
-    path('', home_page, name='home')
+    path('', home_page, name='home'),
+    path('lists/the-only-list-in-the-world/', view_list, name='view-list')
 ]
