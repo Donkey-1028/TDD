b081278812f04c4955c0e9a98da0e876dd55ef08
Donkey-1028 <anbo10283182@gmail.com>
2020-01-16 16:45:15 +0900

    기존 리스트에 아이템을 추가하기 위한 신규 URL 및 뷰

--- lists/urls.py
+++ lists/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import home_page, view_list, new_list
+from .views import home_page, view_list, new_list, add_item
 
 app_name = 'lists'
 
 urlpatterns = [
     path('', home_page, name='home'),
-    path('lists/the-only-list-in-the-world/', view_list, name='view-list'),
+    path('lists/<int:list_id>/', view_list, name='view-list'),
     path('lists/new', new_list, name='new_list'),
+    path('lists/<int:list_id>/add_item', add_item, name='add_item')
 ]
