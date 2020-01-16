79d746e1fff168120a17f916e538a78d31718a11
Donkey-1028 <anbo10283182@gmail.com>
2020-01-16 15:04:31 +0900

    두 개 폼을 위한 URL 적용, views와 tests에 있는 코드 이동, 신규URL

--- lists/urls.py
+++ lists/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import home_page, view_list
+from .views import home_page, view_list, new_list
 
 app_name = 'lists'
 
 urlpatterns = [
     path('', home_page, name='home'),
-    path('lists/the-only-list-in-the-world/', view_list, name='view-list')
+    path('lists/the-only-list-in-the-world/', view_list, name='view-list'),
+    path('lists/new', new_list, name='new_list'),
 ]
