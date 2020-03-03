50d3514895d6d8dffeed9d6eb512649495b07f81
Donkey-1028 <anbo10283182@gmail.com>
2020-03-03 18:51:08 +0900

    skip 제거

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,7 +4,7 @@ from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
-    @skip
+
     def test_cannot_add_empty_list_items(self):
         # 에디스는 메인 페이지에 접속해서 빈 아이템을 실수로 등록하려고 한다.
         # 입력 상자가 비어 있는 상태에서 엔터키를 누른다.
@@ -13,7 +13,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 페이지가 새로고침되고, 빈 아이템을 등록할 수 없다는 에러 메시지가 표시된다.
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, '빈 아이템을 등록할 수 없습니다')
+        self.assertEqual(error.text, "You can't have an empty list item")
         # 다른 아이템을 입력하고 이번에는 정상 처리된다.
         self.browser.find_element_by_id('id_new_item').send_keys('우유 사기\n')
         self.check_for_row_in_list_table('1: 우유 사기')
